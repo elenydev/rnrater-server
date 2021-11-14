9c6d0bd78c66a468e21efeed285bc595f7f3373f
damian <godevdamian@gmail.com>
2021-11-14 17:57:03 +0100

    Fix create user validator

--- validators/user/post/index.ts
+++ validators/user/post/index.ts
@@ -1,13 +1,13 @@
 import { check, body } from "express-validator";
 
-export const createUserValidator = () => [
+export const createUserValidator = () => (
   check("email").trim().isEmail(),
   check("password").trim().exists().isLength({ min: 8 }),
   check("policy").custom((policy) => policy === true),
   check("lastName").trim().exists(),
   check("firstName").trim().exists(),
-  check("nickname").trim().exists(),
-];
+  check("nickname").trim().exists()
+);
 
 export const authenticateUserValidator = () => (
   body("email").isEmail(), body("password").exists().isLength({ min: 8 })
