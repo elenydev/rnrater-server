99bcb93e9bc6d8cc0f8e0a80dcd83fcde293c50c
damian <godevdamian@gmail.com>
2022-02-09 17:45:44 +0100

    Adjusted categories routes

--- routes/categories/index.ts
+++ routes/categories/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import getRoutes from "./get/index";
+import postRoutes from './post/index';
 
 const categoriesRoutes = Router();
 
 categoriesRoutes.use("/categories/get/", getRoutes);
+categoriesRoutes.use('/categories/post/', postRoutes);
 
 export default categoriesRoutes;
